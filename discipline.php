418746aab42ff19ddf9c3fc026c21b8b7907fd87
Tihon <tihon123123@yandex.ru>
2022-04-27 15:50:13 +0700

    4.0

--- discipline.php
+++ discipline.php
@@ -1,4 +1,3 @@
-<?php 
 class Discipline {
 private $id;
 private $title;
@@ -6,12 +5,3 @@ private $title;
 
 public function __construct($i,$t){
 	$this->id = $i;
-	$this->title = $t;
-
-}
-public function display(){
-
-		echo sprintf('%s<br>', $this->title);
-	}
-}
- ?> 
