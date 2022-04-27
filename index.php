2c9a141cab1c733ccbf8bf844bf383dabbefdc64
Tihon <tihon123123@yandex.ru>
2022-04-27 15:43:50 +0700

    2.0

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
-session_start();
-if(!isset($_SESSION['user_name'])){
-	header('Location: login.php');
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<h1>Привет, <a href="profile.php"><?php echo $_SESSION['user_name']; ?></a></h1>
-</body>
-</html> 
+header('Content-Type: text/html; charset=utf-8');
+require_once 'uzbek.php';
+$user = new User();
+$user->set('aaaaa', 'bbbb', '123');
+$user->saveToSession();
+$user->show();
+
+?> 
