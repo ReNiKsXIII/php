8cd8b3e271a9ba89b5b7dae234f69caf87f57507
Tihon <tihon123123@yandex.ru>
2022-04-27 15:48:51 +0700

    3.0

--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <?php 
 require_once 'uzbek.php';
+if (!empty($_POST)){
 $user = new User();
-$user->loadFromSession();
-if ($user->authorize($_POST) === true){
-$_SESSION['authorized'] = 1;
+$user->loadFromForm($_POST);
 $user->saveToSession();
+$_SESSION['authorized'] = 1;
 echo '<script>window.location="index.php"</script>';
 }
 ?>
@@ -13,18 +13,19 @@ echo '<script>window.location="index.php"</script>';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Authorizatoin</title>
+	<title>Registration</title>
 	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
 	<div id='content'>
 	<form action="" method="post">
-	<h3>Authorization</h3>
+	<h3>Registration</h3>
+	<input type="text" name="name" placeholder="Имя"><br>
 	<input type="text" name="login" placeholder="Логин"><br>
 	<input type="text" name="pas" placeholder="Пароль"><br>
 	<input type="submit" name="">
 	</form>
-	<a href="register.php">Регистрация</a>
+
 </div>
 </body>
 </html>
